2d6decdfbdb46c141affada52f918c4448e6122e
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2022-02-09 22:28:25 -0300

    feat: create dto and interface of player

---
+++ src/players/dtos/create-player.dto.ts
@@ -0,0 +1,5 @@
+export interface CreatePlayerDto {
+  readonly phoneNumer: string;
+  readonly email: string;
+  readonly name: string;
+}
--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Post, Put } from '@nestjs/common';
+import { CreatePlayerDto } from './dtos/create-player.dto';
 @Controller('api/v1/players')
-export class PlayersController { }
+export class PlayersController {
+  @Put()
+  async updatePlayer() {
+    return JSON.stringify({
+      name: 'Guilherme Camargo',
+    });
+  }
+
+  @Post()
+  async createPlayer(@Body() createPlayerDto: CreatePlayerDto) {
+    const { name, email, phoneNumer } = createPlayerDto;
+
+    return {
+      name,
+      email,
+      phoneNumer,
+    };
+  }
+}
