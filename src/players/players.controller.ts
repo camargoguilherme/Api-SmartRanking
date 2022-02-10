b8485b771f1a82d32429cd43fe019cb327b683df
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2022-02-10 00:24:58 -0300

    feat: implements create and update methods in players service

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,19 +1,30 @@
-import { Body, Controller, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  ConflictException,
+  Controller,
+  Get,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CreatePlayerDto } from './dtos/create-player.dto';
+import { Player } from './interfaces/player.interface';
 import { PlayersService } from './players.service';
 @Controller('api/v1/players')
 export class PlayersController {
   constructor(private readonly playersService: PlayersService) { }
 
   @Put()
-  async updatePlayer() {
-    return JSON.stringify({
-      name: 'Guilherme Camargo',
-    });
+  async updatePlayer(@Body() createPlayerDto: CreatePlayerDto) {
+    return this.playersService.updatePlayer(createPlayerDto);
   }
 
   @Post()
   async createPlayer(@Body() createPlayerDto: CreatePlayerDto) {
     return this.playersService.createPlayer(createPlayerDto);
+  }
+
+  @Get()
+  async searchPlayers(): Promise<Player[] | Player> {
+    return this.playersService.searchAllPlayers();
   }
 }
--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreatePlayerDto } from './dtos/create-player.dto';
 import { Player } from './interfaces/player.interface';
 import { v1 as uuidv1 } from 'uuid';
@@ -8,13 +13,38 @@ export class PlayersService {
   private readonly logger = new Logger(PlayersService.name);
   private players: Player[] = [];
 
-  async updatePlayer() { }
+  async updatePlayer(createPlayerDto: CreatePlayerDto) {
+    const { email } = createPlayerDto;
+    const hasPlayer = this.players.find((player) => player.email == email);
+    if (!hasPlayer) throw new NotFoundException('Unregistered player');
+
+    return this.update(hasPlayer, createPlayerDto);
+  }
 
   async createPlayer(createPlayerDto: CreatePlayerDto): Promise<Player> {
+    const { email } = createPlayerDto;
+    const hasPlayer = this.players.find((player) => player.email == email);
+
+    if (hasPlayer) throw new ConflictException('Player already registered');
+
     return this.create(createPlayerDto);
   }
 
-  private async create(createPlayerDto: CreatePlayerDto): Promise<Player> {
+  // Find all players
+  async searchAllPlayers(): Promise<Player[]> {
+    return this.players;
+  }
+
+  // Find player by email
+  async searchPlayerByEmail(email: string): Promise<Player> {
+    const _player = this.players.find((player) => player.email === email);
+    return _player;
+  }
+
+  // Create player from parameter 'createPlayerDto'
+  private async create(
+    createPlayerDto: CreatePlayerDto,
+  ): Promise<Player | null> {
     const { name, email, phoneNumber } = createPlayerDto;
     const player: Player = {
       _id: uuidv1(),
@@ -26,7 +56,30 @@ export class PlayersService {
       urlProfilePlayer: 'https://google.com.br/foto123.jpg',
     };
     this.players.push(player);
-    this.logger.log(`player ${JSON.stringify(player, null, 2)}`);
+    this.logger.log(`player ${JSON.stringify(player, null, 2)}`, 'create');
     return player;
+  }
+
+  private async update(
+    playerFound: Player,
+    createPlayerDto: CreatePlayerDto,
+  ): Promise<Player | null> {
+    const { name, phoneNumber, email } = createPlayerDto;
+
+    const playerUpdated = {
+      ...playerFound,
+      name,
+      phoneNumber,
+      email,
+    };
+
+    const indexOfPlayer = this.players.indexOf(playerFound);
+    this.players[indexOfPlayer] = playerUpdated;
+
+    this.logger.log(
+      `player ${JSON.stringify(playerUpdated, null, 2)}`,
+      'update',
+    );
+    return playerUpdated;
   }
 }
